8b0d4aaa62ca658113c3f13671d4d1930a52042f
ayaka95 <172455399+ayaka95@users.noreply.github.com>
2024-11-08 13:23:44 +0000

    [Add]権限

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,18 +1,38 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
   end
 
   def create
     @group = Group.new(group_params)
-    @group.user_id = owner_id
+    @group.owner_id = current_user.id
     if @group.save
       redirect_to groups_path
+    else
+      render 'new'
     end
   end
 
   def index
     @group = Group.all
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @group = Group.find(params[:id])
   end
 
   private
